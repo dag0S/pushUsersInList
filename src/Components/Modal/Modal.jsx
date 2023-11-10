8be8550ccaa95aeb13bb00ad2be0186ba3f875f1
Gosudarev Danila <danidagosudarev@gmail.com>
2023-11-10 22:13:06 +0300

    finish 2

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,11 +2,14 @@ import styled from './Modal.module.css';
 
 const Modal = ({ desc, onCloseModal }) => {
   return (
-    <div className={styled.overhidden} onClick={onCloseModal}>
+    <div>
+      <div className={styled.overhidden} onClick={onCloseModal}></div>
       <div className={styled.modal}>
         <h3 className={styled['modal__title']}>Некорректный ввод</h3>
         <p className={styled['modal__desc']}>{desc}</p>
-        <button className={styled['modal__btn-close']} onClick={onCloseModal}>Закрыть</button>
+        <button className={styled['modal__btn-close']} onClick={onCloseModal}>
+          Закрыть
+        </button>
       </div>
     </div>
   );
